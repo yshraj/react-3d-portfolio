8687b08cf48b93e2c0f6ccbccc93e612af5abb99
yshraj <yashdarjicse@gmail.com>
2024-12-22 18:33:59 +0530

    hero section text updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Yash</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          "Node.js specialist delivering cutting-edge web and automation <br className='sm:block hidden' />
-           solutions with precision and innovation."</p>
+          Full-Stack Developer Automating User Flows & Enhancing User Experiences.
+          </p>
         </div>
       </div>
 
